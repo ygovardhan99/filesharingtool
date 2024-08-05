be06860bc02ecafef2060e582c885ea46b6478d9
ygovardhan99 <ygovardhan99@gmail.com>
2024-08-05 17:11:52 -0500

    Updated Requirements.txt

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ def report(result):
     return render_template('report.html', result=result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
